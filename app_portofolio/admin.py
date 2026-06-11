4d49f3c71b217f5d5ca4df2bb204864696cb5085
bbpomdigital <102672118+xamiuz@users.noreply.github.com>
2026-06-11 12:44:51 +0800

    feat: register ProjectMedia as inline in django admin

--- app_portofolio/admin.py
+++ app_portofolio/admin.py
@@ -1,12 +1,20 @@
 from django.contrib import admin
-from .models import Project, Profile, Experience, Education
+from .models import Project, Profile, Experience, Education, Award, ProjectMedia
+
+class ProjectMediaInline(admin.TabularInline):
+    model = ProjectMedia
+    extra = 1
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'created_at')
     search_fields = ('title', 'tags')
+    inlines = [ProjectMediaInline]
 
 @admin.register(Education)
 class EducationAdmin(admin.ModelAdmin):
     list_display = ('institution', 'degree', 'start_year', 'end_year')
 
+admin.site.register(Profile)
+admin.site.register(Experience)
+admin.site.register(Award)
